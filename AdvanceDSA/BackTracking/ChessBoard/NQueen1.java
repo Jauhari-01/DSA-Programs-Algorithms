aa61e802ec0e61fbc5e32602cf7f634bb5aa0af4
Jauhari-01 <shubham.jauhari009@gmail.com>
2023-03-27 02:09:46 +0530

    added Nqueen-1 problem

---
+++ AdvanceDSA/BackTracking/ChessBoard/NQueen1.java
@@ -0,0 +1,46 @@
+package AdvanceDSA.BackTracking.ChessBoard;
+
+public class NQueen1 {
+    /*
+            //----This code is only for placing n Queens in n rows 
+            //    As we know in a row only one queen we can put in nQueen problem
+            //    for making it easy we should know how we can put a queen in a row
+            //    or how we can place n queens in n rows
+    */
+	public static void main(String[] args) {
+	    int n = 3;
+		char[][] board = new char[n][n];
+		
+		for(int i=0 ; i<n ; i++){
+		    for(int j=0 ; j<n ; j++){
+		        board[i][j] = 'X';
+		    }
+		}
+		
+		nQueen(board,0);
+    }
+    
+    private static void nQueen(char[][] board,int row){
+        if(row == board.length){
+            printBoard(board);
+            return;
+        }
+        
+        //recursion
+        for(int col = 0 ; col < board.length ; col++){
+            board[row][col] = 'Q';
+            nQueen(board,row+1);
+            board[row][col] = 'X';
+        }
+    }
+    
+    private static void printBoard(char[][] b){
+        System.out.println("==========Board==========");
+        for(int i = 0 ; i < b.length ; i++){
+            for(int j=0 ; j< b.length ; j++){
+                System.out.print(b[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
+}
